9e77d18f3106e4bbe1b0e0b7f8395292ab69b66a
NikitaNO <nikita.nagatkin@gmail.com>
2018-01-11 12:48:30 +0200

    Removed default favicon, added index page, initialized sqlite connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const favicon = require('serve-favicon')
 const compress = require('compression')
 const cors = require('cors')
 const helmet = require('helmet')
@@ -15,19 +14,36 @@ const services = require('./services')
 const appHooks = require('./app.hooks')
 const channels = require('./channels')
 
+const Sequelize = require('sequelize')
+const service = require('feathers-sequelize')
+
+const sequelize = new Sequelize('sequelize', '', '', {
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'db.sqlite'),
+  logging: false
+})
+
 const app = express(feathers())
 
 // Load app configuration
 app.configure(configuration())
-// Enable CORS, security, compression, favicon and body parsing
+
+app.set('view engine', 'ejs')
+
 app.use(cors())
 app.use(helmet())
 app.use(compress())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
-// Host the public folder
-app.use('/', express.static(app.get('public')))
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, 'views', 'index.ejs'), {
+    lengthQueue: 0,
+    pendingQueue: []
+  })
+})
 
 // Set up Plugins and providers
 app.configure(express.rest())
@@ -40,7 +56,6 @@ app.configure(services)
 // Set up event channels (see channels.js)
 app.configure(channels)
 
-// Configure a middleware for 404s and the error handler
 app.use(express.notFound())
 app.use(express.errorHandler({ logger }))
 
