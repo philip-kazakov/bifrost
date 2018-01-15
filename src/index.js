4cd0b9098e40bff2bb49000afbe3a44621aa7a9e
NikitaNO <nikita.nagatkin@gmail.com>
2018-01-15 21:29:20 +0200

    Refactored index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ const app = require('./app')
 
 const settings = standardSettings.getSettings()
 
-const server = app.listen({
-  host: settings.server.host,
-  port: settings.server.port
-})
-
 process.on('unhandledRejection', (reason, p) => {
   logger.error('Unhandled Rejection at:', p, 'reason:', reason)
 })
 
-server.on('listening', () => {
-  logger.info('Feathers application started on http://%s:%d', settings.server.host, settings.server.port)
+module.exports = app.listen({
+  host: settings.server.host,
+  port: settings.server.port
+}, () => {
+  logger.info(`Feathers application started on http://${settings.server.host}:${settings.server.port}`)
 })
