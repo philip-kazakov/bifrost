54623b2b1c0a201046a7355ff4c18cdadc23ab12
NikitaNO <nikita.nagatkin@gmail.com>
2018-02-08 18:30:47 +0200

    Added logger util based on new winston@3.0.0

---
+++ src/logger.js
@@ -0,0 +1,15 @@
+const winston = require('winston')
+
+module.exports = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({
+      filename: 'errors.log',
+      level: 'error'
+    }),
+    new winston.transports.File({
+      filename: 'combined.log'
+    })
+  ]
+})
