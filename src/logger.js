80e5f6471d283cb8a2483b48b0f5cb03db8f51dc
NikitaNO <nikita.nagatkin@gmail.com>
2018-02-08 18:32:04 +0200

    Changed winston logger level

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston')
 
 module.exports = winston.createLogger({
-  level: 'info',
+  level: 'debug',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({
